81ee34fc8308db5104b8eb51a53aee535668664e
myasnyankin <myasnyankin.nikita@mail.ru>
2017-10-21 19:25:13 +0300

    new report created

---
+++ Keil_4_Empty_Project/obj/main.d
@@ -0,0 +1,7 @@
+.\obj\main.o: src\main.c
+.\obj\main.o: src\CMSIS/stm32f10x.h
+.\obj\main.o: src\CMSIS/core_cm3.h
+.\obj\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\obj\main.o: .\src\CMSIS\core_cmInstr.h
+.\obj\main.o: .\src\CMSIS\core_cmFunc.h
+.\obj\main.o: src\CMSIS/system_stm32f10x.h
---
+++ Keil_4_Empty_Project/obj/system_stm32f10x.d
@@ -0,0 +1,7 @@
+.\obj\system_stm32f10x.o: src\CMSIS\system_stm32f10x.c
+.\obj\system_stm32f10x.o: src\CMSIS\stm32f10x.h
+.\obj\system_stm32f10x.o: src\CMSIS\core_cm3.h
+.\obj\system_stm32f10x.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\obj\system_stm32f10x.o: .\src\CMSIS\core_cmInstr.h
+.\obj\system_stm32f10x.o: .\src\CMSIS\core_cmFunc.h
+.\obj\system_stm32f10x.o: src\CMSIS\system_stm32f10x.h
